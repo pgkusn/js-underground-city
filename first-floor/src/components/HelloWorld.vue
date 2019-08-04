7d850c2269d08aacf6e7897022096a04cd1b84a1
kenge <pgkusn@gmail.com>
2019-08-04 12:05:06 +0800

    first commit

---
+++ first-floor/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app" class="container">
+        <ul>
+            <li v-for="(item,i) in 9" :key="item" class="section">
+                <div v-if="i===0">
+                    <p>九九乘法表</p>
+                    <p>MULTIPLICATION CHART</p>
+                </div>
+                <ul v-else>
+                    <li>{{i+1}}</li>
+                    <li v-for="(item,j) in 9" :key="item">{{i+1}} x {{j+1}} = {{(i+1)*(j+1)}}</li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style lang="scss" src="@/assets/sass/style.scss"></style>
